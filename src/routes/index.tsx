cdef0d1314bcfbd2d7af81a16711e7ed954c5386
sjh <sjh@gmail.com>
2025-11-11 21:59:32 +0900

    사용자에게 AI 기반 맞춤 축제 추천과 실시간 인기 축제 랭킹을 동시에 제공하는 페이지를 구현합니다. #10

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,12 @@ import MainpageLogin from '../pages/MainpageLogin'
 import MyPage from '../pages/MyPage'
 import CustomerSupportPage from '../pages/CustomerSupportPage'
 import BackgroundLayout from '../Layout/BackgroundLayout'
+import PopularPage from '../pages/PopularPage'
+import AIRecommendPage from '../pages/AIRecommendPage'
+import ScrapEventsPage from '../pages/ScrapEventsPage'
+import TermsPage from '../pages/TermsPage'
+import DeleteAccountPage from '../pages/DeleteAccountPage'
+
 
 export const router = createBrowserRouter([
   {
@@ -22,5 +28,11 @@ export const router = createBrowserRouter([
   },
   { path: '/mypage', element: <MyPage /> },
   { path: '/mainpage', element: <MainpageLogin /> },
-  { path: '/register', element: <RegisterPage /> },
+  { path: '/ai', element: <AIRecommendPage /> },
+  { path: '/popular', element: <PopularPage /> },
+      { path: '/scrap', element: <ScrapEventsPage /> },
+      { path: '/term', element: <TermsPage /> },
+      { path: '/register', element: <RegisterPage /> },
+      { path: '/delete', element: <DeleteAccountPage /> },
+
 ])
