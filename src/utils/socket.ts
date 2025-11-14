98ade3c6d605f8fe464f9f8999fa4aae37fc5f17
sjh <sjh@gmail.com>
2025-11-15 06:47:40 +0900

    socket uri 수정입니다.

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -7,7 +7,7 @@ let stompClient: CompatClient | null = null;
 export const connectStomp = (): Promise<CompatClient> => {
   return new Promise((resolve, reject) => {
     const accessToken = localStorage.getItem('access_token');
-    const socket = new SockJS('https://13.125.224.67.nip.io/ws-stomp');
+    const socket = new SockJS('https://api.gyeonggifesta.site/ws-stomp');
     stompClient = Stomp.over(socket);
 
     stompClient.connect(
