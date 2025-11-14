c8de8b6becaaaafcd143562499f6e39545526d7e
sjh <sjh@gmail.com>
2025-11-15 04:55:29 +0900

    불필요 제목 제거완료입니다.

--- src/pages/MainpageLogin.tsx
+++ src/pages/MainpageLogin.tsx
@@ -88,8 +88,6 @@ const MainpageLogin = () => {
           <FestivalCard
             eventId={festival.eventId}
             commentCount={festival.comments}
-            mainText={festival.title}
-            subText={festival.category}
             festivalName={festival.title}
             dateRange={`${festival.startDate} ~ ${festival.endDate}`}
             price={festival.isFree === '무료' ? '무료' : '유료'}
